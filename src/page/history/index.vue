30424e37e4d1a90a68154c4a3ac74d7052346eac
liumapp <liumapp.com@gmail.com>
2017-10-25 15:03:56 +0800

    一览$route真身

--- src/page/history/index.vue
+++ src/page/history/index.vue
@@ -1,6 +1,16 @@
 <template>
-<div class="product">
+<div class="product" v-on:click="init">
   this is history index , which id is {{ $route.params.id }} , name is {{ $route.params.name }}
 </div>
 </template>
 
+<script>
+  export default {
+    name: 'History',
+    methods: {
+      init: function (event) {
+        console.log(this.$route)
+      }
+    }
+  }
+</script>
