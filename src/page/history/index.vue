fdc63d689180e4433e35ba41a29da07aefe94c69
liumapp <liumapp.com@gmail.com>
2017-10-25 17:35:14 +0800

    tmp

--- src/page/history/index.vue
+++ src/page/history/index.vue
@@ -1,15 +1,32 @@
 <template>
 <div class="product" v-on:click="init">
-  this is history index , which id is {{ $route.params.id }} , name is {{ $route.params.name }}
+  this is history index , which id is {{ id }} , name is {{ name }}
+
+  <button v-on:click="addId">add id</button>
+  &nbsp;
+  <button v-on:click="testPush">route-push</button>
+  <router-view></router-view>
 </div>
 </template>
 
 <script>
   export default {
-    name: 'History',
+    name: 'history',
+    data () {
+      return {
+        id: Number(this.$route.params.id),
+        name: this.$route.params.name
+      }
+    },
     methods: {
       init: function (event) {
         console.log(this.$route)
+      },
+      addId: function (event) {
+        this.id += 1
+      },
+      testPush: function (event) {
+        this.$router.push({ path: `/history/${this.id}/${this.name}` })
       }
     }
   }
