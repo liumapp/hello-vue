aa1180c2540aa6f21f0a751415e885cedbe00974
liumapp <liumapp.com@gmail.com>
2017-10-26 10:22:33 +0800

    多路由的情况

--- src/page/multyview/index.vue
+++ src/page/multyview/index.vue
@@ -1,12 +1,15 @@
 <template>
-<div class="product" v-on:click="init">
-  this is history index , which id is {{ id }} , name is {{ name }}
+  <div class="product" v-on:click="init">
+    this is history index , which id is {{ id }} , name is {{ name }}
 
-  <button v-on:click="addId">add id</button>
-  &nbsp;
-  <button v-on:click="testPush">route-push</button>
-  <router-view></router-view>
-</div>
+    <button v-on:click="addId">add id</button>
+    &nbsp;
+    <button v-on:click="testPush">route-push</button>
+    <br>
+    <router-view class="view one"></router-view>
+    <router-view class="view two" name="a"></router-view>
+    <router-view class="view three" name="b"></router-view>
+  </div>
 </template>
 
 <script>
