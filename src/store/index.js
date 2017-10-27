bd3ac0e8e75acdd515bc159d935f51e0c2cb8b9d
liumapp <liumapp.com@gmail.com>
2017-10-27 16:33:52 +0800

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,18 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    globalCount: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+export default {
+  store
+}
